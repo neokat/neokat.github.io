cb96ffd3a421f6950d9e7e6f29cb270507b1bc7d
Kathryn Reagan <kathryn.reagan@rewardstyle.com>
2021-05-26 18:17:46 -0500

    gotta assign itttt

--- scores.js
+++ scores.js
@@ -16,7 +16,7 @@ function build_table(score_data) {
         let score = score_data[i];
         console.log(score);
         console.log(i);
-        table_string.concat(
+        table_string = table_string.concat(
             "<tr><td style=\"text-align: center !important;\"><img src=\"",
             score["team_logo"],
             "\" width=\"50px\"></td><td>",
@@ -25,7 +25,7 @@ function build_table(score_data) {
             score["yyb_wins"],
             "</td><td>1</td><td>100</td><td>370</td><td>3,500</td><td>10,714</td><td>All-Star</td></tr>\n"
         );
-        console.log("i = ".concat(i.toString(), "table string = ", table_string))
+        console.log("i = ".concat(i.toString(), " and the table string = ", table_string))
     }
     return table_string;
 }
