76ffcdc58f6b32c43f5a95ebb46e437ddd773227
Kathryn Reagan <kathryn.reagan@rewardstyle.com>
2021-05-26 17:51:08 -0500

    fix my broken code

--- scores.js
+++ scores.js
@@ -3,7 +3,7 @@ $().ready(function(){
     console.log(data);
     $("#last_updated").html(data["last_updated"]);
 
-    scores_html = build_table(data["scores"]);
+    let scores_html = build_table(data["scores"]);
 
     $("#score_contents").html(scores_html);
 
@@ -11,8 +11,10 @@ $().ready(function(){
 });
 
 function build_table(score_data) {
-    for (score in score_data) {
-        return string.concat(
+    let table_string = "";
+    for (let i = 0; i < score_data.length; i++) {
+        let score = score_data[i];
+        table_string.concat(
             "<tr><td style=\"text-align: center !important;\"><img src=\"",
             score["team_logo"],
             "\" width=\"50px\"></td><td>",
@@ -20,6 +22,6 @@ function build_table(score_data) {
             "</td><td>",
             score["yyb_wins"],
             "</td><td>1</td><td>100</td><td>370</td><td>3,500</td><td>10,714</td><td>All-Star</td></tr>\n"
-        )
+        );
     }
 }
