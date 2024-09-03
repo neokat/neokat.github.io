6dac6f2bdb10395356b7a206f983f14b07d69bb6
Kat <kathryn.m.reagan@gmail.com>
2024-09-02 21:35:36 -0500

    revamp potato pile

--- python/generate_potato_pile.py
+++ python/generate_potato_pile.py
@@ -10,10 +10,23 @@ with open('../data/potato_counts.csv', mode='r') as counts_file:
         potatoes = int(row['potatoes'])
         team = row['team']
 
-        for _ in range(potatoes):
-            pile.append({'username': username, 'team': team, 'potatoes': potatoes})
+        for _ in range(int(row['rank'])):
+            pile.append({'username': username, 'team': team, 'type': 'rank'})
+        for _ in range(int(row['pledge'])):
+            pile.append({'username': username, 'team': team, 'type': 'pledge'})
+        for _ in range(int(row['activities'])):
+            pile.append({'username': username, 'team': team, 'type': 'activities'})
+        for _ in range(int(row['chatting'])):
+            pile.append({'username': username, 'team': team, 'type': 'chatting'})
+        for _ in range(int(row['marathon'])):
+            pile.append({'username': username, 'team': team, 'type': 'marathon'})
+        for _ in range(int(row['mvp'])):
+            pile.append({'username': username, 'team': team, 'type': 'mvp'})
+        for _ in range(int(row['busybee'])):
+            pile.append({'username': username, 'team': team, 'type': 'busybee'})
 
 random.shuffle(pile)
+print(f"There are {len(pile)} potatoes in the pile")
 
 with open('../data/potato_pile.json', 'w') as pile_file:
     json.dump(pile, pile_file, indent=4)
